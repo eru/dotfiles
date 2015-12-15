5794289c01e8a7bb87602feb01e9b4a1f964e118
Kazuki Hashimoto <eru.tndl@gmail.com>
2015-12-15 16:28:22 +0900

    いつまでzgenで消耗してんの？

---
+++ _zsh/zplug.zsh
@@ -0,0 +1,88 @@
+# load zplug
+source ~/.zplug/zplug
+
+# plugins
+zplug "plugins/brew", from:oh-my-zsh
+zplug "plugins/bundler", from:oh-my-zsh
+zplug "plugins/cake", from:oh-my-zsh
+zplug "plugins/cakephp3", from:oh-my-zsh
+zplug "plugins/coffee", from:oh-my-zsh
+zplug "plugins/cpanm", from:oh-my-zsh
+zplug "plugins/composer", from:oh-my-zsh
+zplug "plugins/docker", from:oh-my-zsh
+zplug "plugins/docker-compose", from:oh-my-zsh
+zplug "plugins/extract", from:oh-my-zsh
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/git-flow", from:oh-my-zsh
+zplug "plugins/heroku", from:oh-my-zsh
+zplug "plugins/history", from:oh-my-zsh
+zplug "plugins/lein", from:oh-my-zsh
+zplug "plugins/mosh", from:oh-my-zsh
+zplug "plugins/node", from:oh-my-zsh
+zplug "plugins/npm", from:oh-my-zsh
+zplug "plugins/pip", from:oh-my-zsh
+zplug "plugins/pyenv", from:oh-my-zsh
+zplug "plugins/rsync", from:oh-my-zsh
+zplug "plugins/tmux", from:oh-my-zsh
+zplug "plugins/vagrant", from:oh-my-zsh
+zplug "plugins/web-search", from:oh-my-zsh
+zplug "plugins/zsh_reload", from:oh-my-zsh
+
+# OS別の設定
+case "${OSTYPE}" in
+  darwin*)
+  zplug "plugins/brew-cask", from:oh-my-zsh
+  zplug "plugins/osx", from:oh-my-zsh
+  zplug "plugins/xcode", from:oh-my-zsh
+  ;;
+  linux*)
+  zplug "plugins/systemd", from:oh-my-zsh
+  zplug "plugins/yum", from:oh-my-zsh
+  zplug "plugins/fedora", from:oh-my-zsh
+  ;;
+esac
+
+# Syntax highlighting oh-my-zsh.
+zplug "zsh-users/zsh-syntax-highlighting", nice:8
+
+# Completions oh-my-zsh.
+zplug "zsh-users/zsh-completions"
+
+# fuzzy finder and next generation cd
+zplug "junegunn/fzf", do:"./install --all --key-bindings --no-completion --no-update-rc" \
+  | zplug "b4b4r07/enhancd", of:enhancd.sh
+
+# autosuggestions should be loaded last
+zplug "tarruda/zsh-autosuggestions", nice:9
+
+# emoji completion
+zplug "stedolan/jq", as:command, file:jq, from:gh-r \
+  | zplug "b4b4r07/emoji-cli"
+
+# emoji on cli
+zplug "mrowa44/emojify", as:command, file:emojify
+
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose; then
+  printf "Install? [y/N]: "
+  if read -q; then
+    echo; zplug install
+  fi
+fi
+
+# Then, source plugins and add commands to $PATH
+zplug load
+
+# Enable fzf key bindings
+if zplug check "junegunn/fzf"; then
+  [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+fi
+
+# Enable autosuggestions automatically.
+if zplug check "tarruda/zsh-autosuggestions"; then
+  zle-line-init() {
+    zle autosuggest-start
+  }
+  zle -N zle-line-init
+  bindkey '^f' vi-end-of-line
+fi
