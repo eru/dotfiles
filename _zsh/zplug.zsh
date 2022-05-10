21b679c67efe21e7320ca24749bb8f9b720d8521
Kazuki Hashimoto <k.hashimoto@creditengine.jp>
2022-05-10 13:56:51 +0900

    zsh plugins整理

--- _zsh/zplug.zsh
+++ _zsh/zplug.zsh
@@ -2,27 +2,26 @@
 source $HOME/.zplug/init.zsh
 
 # plugins
-zplug "plugins/bundler", from:oh-my-zsh
-zplug "plugins/cpanm", from:oh-my-zsh, if:"which cpanm"
-zplug "plugins/composer", from:oh-my-zsh, defer:2
-zplug "plugins/docker", from:oh-my-zsh, if:"which docker"
 zplug "plugins/docker-compose", from:oh-my-zsh, if:"which docker-compose"
 zplug "plugins/extract", from:oh-my-zsh
 zplug "plugins/dnf", from:oh-my-zsh, if:"which dnf"
 zplug "plugins/git", from:oh-my-zsh, defer:2
 zplug "plugins/git-flow", from:oh-my-zsh
 zplug "plugins/macos", from:oh-my-zsh, if:"[[ $OSTYPE == *darwin* ]]", defer:2
-zplug "plugins/pip", from:oh-my-zsh, if:"which pip"
 
-# Completions oh-my-zsh.
 zplug "zsh-users/zsh-completions"
 
-# fuzzy finder and next generation cd
+# fuzzy finder
 zplug "junegunn/fzf", hook-build:"./install --all --key-bindings --no-completion --no-update-rc"
+
+# require fuzzy finders
+# next generation cd
 zplug "b4b4r07/enhancd", use:init.sh, on:"junegunn/fzf"
+# history search
+zplug "joshskidmore/zsh-fzf-history-search", on:"junegunn/fzf"
 
 # autosuggestions should be loaded before Syntax highlighting and after compinit
-zplug "tarruda/zsh-autosuggestions", defer:3
+zplug "zsh-users/zsh-autosuggestions", defer:3
 
 # Syntax highlighting oh-my-zsh.
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
@@ -35,7 +34,7 @@ zplug "b4b4r07/emoji-cli", on:"stedolan/jq"
 zplug "mrowa44/emojify", as:command, use:"emojify"
 
 # phpctags
-zplug "vim-php/phpctags", hook-build:"composer install && make clean && make", as:command, use:"build/phpctags.phar", rename-to:phpctags, if:"which php"
+# zplug "vim-php/phpctags", hook-build:"composer install && make clean && make", as:command, use:"build/phpctags.phar", rename-to:phpctags, if:"which php"
 
 # self management
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
