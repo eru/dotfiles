b47b8533dd8080174d8e9d9903d1aca0db0812b8
Kazuki Hashimoto <k.hashimoto@creditengine.jp>
2024-10-15 14:28:01 +0900

    :wrench: Change keymap

--- _config/nvim/lua/plugins/lspsaga.lua
+++ _config/nvim/lua/plugins/lspsaga.lua
@@ -10,6 +10,10 @@ return {
     vim.keymap.set("n", "gn", "<cmd>Lspsaga diagnostic_jump_next<CR>")
     vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>")
     vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename ++project<CR>")
+    vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
+    vim.keymap.set("n", "<leader>gd", "<cmd>Lspsaga peek_definition<CR>")
+    vim.keymap.set("n", "gy", "<cmd>Lspsaga goto_type_definition<CR>")
+    vim.keymap.set("n", "<leader>gy", "<cmd>Lspsaga peek_type_definition<CR>")
 
     require("lspsaga").setup({
       symbol_in_winbar = {
--- _config/nvim/lua/plugins/nvim-lspconfig.lua
+++ _config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -15,7 +15,7 @@ return {
         local opts = { buffer = ev.buf }
 
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
         -- vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
         vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
@@ -24,7 +24,7 @@ return {
         vim.keymap.set("n", "<leader>wl", function()
           print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
         end, opts)
-        vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, opts)
+        -- vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, opts)
         -- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
         -- vim.keymap.set("n", "<leader>ac", vim.lsp.buf.code_action, opts)
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
